8197d1f546e68b7ab9769f1fd4abb71a2ed5f267
TaiJi009 <zbyingwusuozhu@gmail.com>
2026-01-29 15:04:00 +0800

    V0.0.0-2026.1.29
    清空

--- dashboard_web/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:dashboard_web/main.dart';
-
-void main() {
-  testWidgets('App builds', (tester) async {
-    await tester.pumpWidget(const DashboardApp());
-    expect(find.text('概览'), findsAtLeast(1));
-  });
-}
